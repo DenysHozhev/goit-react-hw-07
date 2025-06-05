c1cbeb76f702459b79eb911ee028bb6d90e35013
Denys <gozhievd@gmail.com>
2025-06-05 15:16:10 +0200

    thre

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./components/App";
 
 import { Provider } from "react-redux";
-import { store } from "./redux/Store";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
